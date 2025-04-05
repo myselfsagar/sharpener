ca3e6c3acc16b66a26e8ecef40c034dc5a2db41b
myselfsagar <ssahu6244@gmail.com>
2025-04-05 20:17:13 +0530

    Router and Filters

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const app = express();
+const productsRouter = require("./routes/products.js");
+const categoriesRouter = require("./routes/categories.js");
 
 app.use((req, res, next) => {
   console.log(`${req.method} request made to ${req.url}`);
   next();
 });
 
-app.get("/products", (req, res) => {
-  res.send("<h1>Here is the list of all products.</h1>");
-});
-
-app.post("/products", (req, res) => {
-  res.send("<h1>A new product has been added.</h1>");
-});
-
-app.get("/categories", (req, res) => {
-  res.send("<h1>Here is the list of all categories.</h1>");
-});
-
-app.post("/categories", (req, res) => {
-  res.send("<h1>A new category has been created.</h1>");
-});
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
 
 const PORT = 4000;
 app.listen(PORT, () => {
