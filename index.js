5d40578b69b2fbaae9f9873792ea9d0a1a5db0a9
myselfsagar <ssahu6244@gmail.com>
2025-04-05 19:35:33 +0530

    Question-Routes

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
 
-const userMiddleware = (req, res, next) => {
-  req.user = "Guest";
-  next();
-};
+app.get("/orders", (req, res) => {
+  res.send("<h1>Here is the list of all orders.</h1>");
+});
 
-app.get("/welcome", userMiddleware, (req, res) => {
-  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+app.post("/orders", (req, res) => {
+  res.send("<h1>A new order has been created.</h1>");
+});
+
+app.get("/users", (req, res) => {
+  res.send("<h1>Here is the list of all users.</h1>");
+});
+
+app.post("/users", (req, res) => {
+  res.send("<h1>A new user has been added.</h1>");
 });
 
 const PORT = 3000;
 app.listen(3000, () => {
-  console.log(
-    `Server is up and running on port ${PORT}! Ready to handle requests.`
-  );
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
