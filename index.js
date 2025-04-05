c8f25dc95ec966df0b0908979193bc5bffddb7b6
myselfsagar <ssahu6244@gmail.com>
2025-04-05 17:30:21 +0530

    Create Your Own Middleware and Routing

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const userMiddleware = (req, res, next) => {
+  req.user = "Guest";
+  next();
+};
+
+app.get("/welcome", userMiddleware, (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
 const PORT = 3000;
 app.listen(3000, () => {
   console.log(
